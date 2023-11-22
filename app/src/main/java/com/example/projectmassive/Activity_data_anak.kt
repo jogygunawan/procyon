cbdecc3d70a45fbb69fffe032fa8c740702c83d2
Jogy Gunawan <jogygunawan@gmail.com>
2023-11-23 01:11:46 +0900

    version 1.5

---
+++ app/src/main/java/com/example/projectmassive/Activity_data_anak.kt
@@ -0,0 +1,11 @@
+package com.example.projectmassive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_data_anak : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_anak)
+    }
+}
